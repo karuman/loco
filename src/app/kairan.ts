71dc2e4235e26da9efb250614623604ffa84bba8
karuman <karuman@gmail.com>
2023-02-18 17:26:43 +0900

    update class

--- src/app/kairan.ts
+++ src/app/kairan.ts
@@ -1,4 +1,11 @@
 export interface kairanLst {
     title: string;
     note: string;
+    year: string;
+  }
+
+export interface kairan {
+    note: string;
+    idx: string;
+    img_url: string;
   }
