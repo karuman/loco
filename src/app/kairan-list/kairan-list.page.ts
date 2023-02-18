5f11062a1f74625aa9792ece5a27530ff8180d0f
karuman <karuman@gmail.com>
2023-02-18 11:11:09 +0900

    get kairan list from firestore

--- src/app/kairan-list/kairan-list.page.ts
+++ src/app/kairan-list/kairan-list.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { kairanLst } from '../kairan';
 import { KairanService } from '../kairan.service';
+import { AngularFirestore ,AngularFirestoreCollection} from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-kairan-list',
@@ -12,11 +13,17 @@ export class KairanListPage implements OnInit {
 
   cards: Array<kairanLst>=[];
 
-  constructor(public navCtrl: NavController,private kairanService: KairanService) {
+  constructor(public navCtrl: NavController,private kairanService: KairanService,private firestore: AngularFirestore) {
   }
 
   ngOnInit() {
-    this.cards = this.kairanService.getKairanLst();
+//    this.cards = this.kairanService.getKairanLst();
+//    this.cards =  this.firestore.collection('kairan');
+    this.firestore.collection('kairan',ref=> ref.orderBy('note','desc')).valueChanges().subscribe(cards => {
+
+      this.cards = cards as kairanLst[];
+ 
+    });
   }
   gotoKairan(opt:any){
     this.navCtrl.navigateForward(["kairan"],{ queryParams: { order: opt } });
